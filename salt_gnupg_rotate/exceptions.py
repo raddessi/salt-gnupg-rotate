cdbb66c1fc2d7b6af801211eec98c397250f3cf4
Ryan Addessi <raddessi@users.noreply.github.com>
2024-01-25 22:20:10 -0700

    fix: black

--- salt_gnupg_rotate/exceptions.py
+++ salt_gnupg_rotate/exceptions.py
@@ -3,9 +3,11 @@
 
 class DecryptionError(ValueError):
     """Error while trying to decrypt an encoded block of text."""
+
     pass
 
 
 class EncryptionError(ValueError):
     """Error whilte trying to encrypt a block of text."""
+
     pass
