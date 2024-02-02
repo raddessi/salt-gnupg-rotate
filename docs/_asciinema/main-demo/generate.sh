a2804518e0ba8b85e9c4a397dbf4baafc6ae5948
Ryan Addessi <raddessi@users.noreply.github.com>
2024-02-02 10:32:34 -0700

    better

--- docs/_asciinema/main-demo/generate.sh
+++ docs/_asciinema/main-demo/generate.sh
@@ -7,14 +7,14 @@ docker build -t your_image_name .
 
 docker run -it \
     --rm \
+    --sig-proxy \
     --name myimage \
     --hostname demo \
-    -v ./:/root/:z \
     -v ../../../:/salt-gnupg-rotate/:z \
     your_image_name \
-    bash -c 'pip install --no-cache /salt-gnupg-rotate/; clear; asciinema-automation --delay 1 --standard-deviation 10 --wait 1000 --asciinema-arguments " --overwrite --rows 24 --cols 120 --env SHELL,TERM,PATH -c \"env bash\"" script.expect output.cast'
+    bash -c 'pip install --no-cache /salt-gnupg-rotate/; clear; asciinema-automation --asciinema-arguments " --overwrite --rows 24 --cols 120 --env SHELL,TERM,PATH -c \"env bash\"" /salt-gnupg-rotate/docs/_asciinema/main-demo/script.expect /salt-gnupg-rotate/docs/_asciinema/main-demo/output.cast'
 
-    # bash -c 'pip install /salt-gnupg-rotate/; clear; asciinema-automation --delay 1 --standard-deviation 10 --wait 1000 --asciinema-arguments " --overwrite -c \"env -i bash\"" script.sh output.cast'
+    # bash -c 'pip install /salt-gnupg-rotate/; clear; asciinema-automation --delay 1 --standard-deviation 10 --wait 1000 --asciinema-arguments " --overwrite -c \"env -i bash\"" /salt-gnupg-rotate/docs/_asciinema/main-demo/script.sh /salt-gnupg-rotate/docs/_asciinema/main-demo/output.cast'
     # asciinema-automation
 
 echo "Done recording! Playing back now."
