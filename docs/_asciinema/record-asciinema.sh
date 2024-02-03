efc8aed70a90576dcc3d92d1a82fd47660f4dc20
Ryan Addessi <raddessi@users.noreply.github.com>
2024-02-02 22:56:56 -0700

    better

--- docs/_asciinema/record-asciinema.sh
+++ docs/_asciinema/record-asciinema.sh
@@ -19,7 +19,7 @@ fi
 # Defaults
 DOCKER_IMAGE=salt-gnupg-rotate-asciinema
 GIT_TOP_DIRPATH=$(git rev-parse --show-toplevel)
-rebuild=true
+REBUILD=true
 
 # Loop through all arguments
 while [[ ${#} -gt 0 ]]; do
@@ -33,15 +33,15 @@ while [[ ${#} -gt 0 ]]; do
         exit 0
         ;;
     -f | --file)
-        fpath="$(realpath $2)"
-        if ! [ -f "${fpath}" ]; then
-            echo "No such file ${fpath}"
+        FPATH="$(realpath $2)"
+        if ! [ -f "${FPATH}" ]; then
+            echo "No such file ${FPATH}"
             exit 4
         fi
         shift
         ;;
     --no-rebuild)
-        rebuild=false
+        REBUILD=false
         ;;
     *)
         # Handle other arguments
@@ -54,16 +54,16 @@ done
 
 # we will eventually write the output cast to the same directory as the expect script,
 # with the same name
-output_dirpath=$(dirname "$(realpath ${fpath})")
-output_fname=$(basename "${fpath}".cast)
+OUTPUT_DIRPATH=$(dirname "$(realpath ${FPATH})")
+OUTPUT_FNAME=$(basename "${FPATH}".cast)
 
 # change working dir to the top level of the git repo
 cd "${GIT_TOP_DIRPATH}" || exit 2
 
 echo -e "Cleaning up previous recording casts ..."
-rm -f "${output_dirpath}/${output_fname}"
+rm -f "${OUTPUT_DIRPATH}/${OUTPUT_FNAME}"
 
-if [ ${rebuild} == "true" ]; then
+if [ ${REBUILD} == "true" ]; then
     echo -e "\nBuilding docker image ..."
     docker build -t ${DOCKER_IMAGE} -f ./Dockerfile.asciinema .
 fi
@@ -75,7 +75,7 @@ docker run \
     --sig-proxy \
     --name myimage \
     --hostname demo \
-    -v "${output_dirpath}:/output/:z" \
+    -v "${OUTPUT_DIRPATH}:/output/:z" \
     ${DOCKER_IMAGE} \
     bash -c " \
         asciinema-automation \
@@ -88,9 +88,9 @@ docker run \
                 --env SHELL,TERM,PATH \
                 -c \"env bash\" \
             ' \
-            '$(realpath --relative-to=${GIT_TOP_DIRPATH} ${fpath})' \
-            '/output/${output_fname}' \
+            '$(realpath --relative-to=${GIT_TOP_DIRPATH} ${FPATH})' \
+            '/output/${OUTPUT_FNAME}' \
     "
 
-echo -e "\nDone recording! Playing back now:"
-asciinema play "${output_dirpath}/${output_fname}"
+echo -e "\nDone recording! Cast saved to ${OUTPUT_DIRPATH}/${OUTPUT_FNAME}. Playing back now:"
+asciinema play "${OUTPUT_DIRPATH}/${OUTPUT_FNAME}"
